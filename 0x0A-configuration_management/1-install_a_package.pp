6a8b1775cf906154af9e8589b10cd2d774b1ffc6
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-05-08 00:31:21 +0100

    MOdified the code for teask 1{

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,9 @@ package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
+# Install an especific version of werkzeug (2.1.0)
+package {'werkzeug':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
